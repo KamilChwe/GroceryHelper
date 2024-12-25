386ee9d7940cfbd02825a2f05460c1799b40545f
Kamil Chwedura <kchwedura@gmail.com>
2024-12-25 20:33:03 +0000

    Start of Item List
    - Started adding the item list

--- root/public/js/functions.js
+++ root/public/js/functions.js
@@ -1,21 +1,35 @@
+// Test function
 async function ping() {
+    // Try to run the code in the brackets
     try{
+        // Create a constant var named response
+        // response will await for /ping to be called
         const response = await fetch('/ping', {
+            // Utilise the POST method
+            // Set the header 'Content-Type' to 'application/json'
+            // Finally set the body of the response to "Button Clicked!"
             method: 'POST',
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify({message: 'Button Clicked!'})
         });
-
+        // Createa var named result
+        // This var will take the JSON and turn it into a JS object
         const result = await response.json();
+        // Create an alert window
         alert(`NodeJS responded with:${result.response}`);
     }catch(error){
+        // Display error log to the console
         console.error("Error Occured: ", error);
     }
 }
 
-async function addItems(){
+// Function that adds an item to the database (one item)
+async function addItem(){
     try{
+        // Listen for the /add command
         const response = await fetch('/add', {
+            // Setup the response using POST and JSON
+            // With the body 
             method: 'POST',
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify({message: 'Add Items'})
@@ -28,6 +42,7 @@ async function addItems(){
     }
 }
 
+// Function that deletes items in the database (one or more)
 async function delItems(){
     try{
         const response = await fetch('/del', {
